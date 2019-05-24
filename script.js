2cc15df67b05e6476beab5768e445a071bf53916
Laurens Koppenol <lkoppenol@gmail.com>
2019-05-24 13:42:17 +0200

    fixed tag

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ function createMarkers(data){
 
 function loadTileLayer(){
     tile_layer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a> Extension by <a href="http://xomnia.com">Xomnia</a>',
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
         maxZoom: 18,
         id: 'mapbox-id',
         accessToken: 'mapbox-access-token'
